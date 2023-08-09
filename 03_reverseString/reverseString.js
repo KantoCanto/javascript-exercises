1af16f10701c4bbc95bc8dddcd1eceab2e358288
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-09 15:17:49 +0100

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+//split() method splits a string object into an array of strings by separating the string into sub strings
+//reverse() method reverses an array in place. The first array element becomes the last and the last becomes the first
+//join() method joins all elements of an array into a string
+
+const reverseString = function(string) {
+    
+    let stringToArray = string.split("");
+    let reverseString = stringToArray.reverse();
+    let result = reverseString.join("");
+
+    return result
 
 };
 
