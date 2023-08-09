dd6b971da0cf3f6aaeb451dc331c54f55b1cf29d
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-09 15:28:28 +0100

    commented on thought process

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,19 @@
 const removeFromArray = function(array, ...args) {
+
+    //new array that will save the new items
      const newArray = [];
 
+     //forEach goes through every item inside of the array
      array.forEach((item) => {
+
+        //if the arguments and the original array items don't match, push those 
+        //to he new array
         if (!args.includes(item)) {
             newArray.push(item)
         }
      });
 
+     //return that array
      return newArray;
 };
 
