494a4db337a2043e1b5d3f89af3704f8f1fdd27c
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-09 15:49:46 +0100

    can't run tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let max;
+    let min;
+    let totalSum;
+
+   if(int2 > int1){
+     max = int2;
+     min = int1;
+   }else{
+     max = int1;
+     min = int2;
+   }
+
+    if(max == min){
+        totalSum = min + max;
+        return totalSum;
+    }else{
+        while(max > min){
+            totalSum = max + (max-1)
+            max = max--;
+        }
+        return totalSum;
+    }
+   
 };
 
 // Do not edit below this line
