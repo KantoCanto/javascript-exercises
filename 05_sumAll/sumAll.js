058040188017f2489d05e3a30e82bee2000a9bb0
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-10 12:39:39 +0100

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,28 +1,29 @@
 const sumAll = function(int1, int2) {
+  
+  //check to see if parameters given are integer numbers. if any of them isn't, return error
+  if(!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
 
-    let max;
-    let min;
-    let totalSum;
+  //check if any of them is lower than zero. if so return error
+  if(int1 < 0 || int2 <0) return "ERROR";
 
-   if(int2 > int1){
-     max = int2;
-     min = int1;
-   }else{
-     max = int1;
-     min = int2;
-   }
+  let sum = 0;
 
-    if(max == min){
-        totalSum = min + max;
-        return totalSum;
-    }else{
-        while(max > min){
-            totalSum = max + (max-1)
-            max = max--;
-        }
-        return totalSum;
+  //attributes max and min to the respective numbers
+  let max = Math.max(int1, int2);
+  let min = Math.min(int1, int2);
+  
+  if(max != min){
+
+    //starting with the min number, we add the rest of the numbers to it, 1 by 1, until we reach max
+    for(let i = min; i <= max; i++){
+      sum += i;
     }
-   
+    return sum;
+  }else if(min == max){
+    sum = min + max;
+    return sum;
+  }
+
 };
 
 // Do not edit below this line
