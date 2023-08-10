79cf7bcc513244b22e243822858a186d62c64f53
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-10 13:20:04 +0100

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //is divisible by 4
+    if(year % 4 == 0){
+        console.log("divisible by 4");
+          //divisible by 100 are not leapYears UNLESS they are also divisible by 400
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                return true;
+            }else if(year % 400 != 0 ){
+                return false;
+            }
+        }else{
+           return true;
+        }
+    }
+    else{
+        return false;
+    };
+
+  
 
 };
 
