48d3ee938e80b8c95ae1940b19b4c5a334e54ebc
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-10 13:53:57 +0100

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
+const ftoc = function(tempf) {
 
+  let celsius = (tempf - 32) * (5/9);
+  roundedCelcius = Math.round( celsius * 10) / 10;
+  console.log(celsius, roundedCelcius);
+  return roundedCelcius;
 };
 
-const ctof = function() {
+const ctof = function(tempc) {
 
+  let farenheit = (tempc * (9/5) + 32);
+  roundedFarenheit = Math.round(farenheit * 10) / 10;
+  console.log(farenheit, roundedFarenheit);
+  return roundedFarenheit; 
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
+  ftoc, 
   ctof
 };
