e7fd8059842c3a98f68bf5988deefedf92912755
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-11 13:29:06 +0100

    solved -> need to work on array.reduce

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let addition = num1 + num2;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let subtraction = num1 - num2;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	array.forEach((number) => {
+    total = total + number;
+  })
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let pow = num1 ** num2;
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let factor = 1;
+  if(num1 == 0){
+    factor = 1;
+    return factor;
+  }
+  for(let i = 1; i<= num1; i++){
+    factor = factor * i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
