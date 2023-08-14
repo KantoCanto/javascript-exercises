5fadc30731e9d97f75a6cb3caf26ca1510c2f0e9
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-14 11:29:43 +0100

    solved + commented

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,9 @@
 const getTheTitles = function(books) {
 
+    //new array that will store the titles
     let titles = [];
 
+    //cycle through each array element, and push the book title to the new titles array
     for(let i = 0; i < books.length ; i++){
         let title = books[i].title;
         titles.push(title);
